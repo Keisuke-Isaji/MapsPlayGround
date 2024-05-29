b5e446991c557bd23e44aaee8124c79393380469
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-05-29 10:15:22 +0900

    Initial Commit

---
+++ MapsPlayGround/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapsPlayGround
+//
+//  Created by 伊佐治恵介 on 2024/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapsPlayGround/MapsPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsPlayGroundApp.swift
+//  MapsPlayGround
+//
+//  Created by 伊佐治恵介 on 2024/05/29.
+//
+
+import SwiftUI
+
+@main
+struct MapsPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
